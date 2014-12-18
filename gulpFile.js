4ba8d2daeeb8368844318057a0933bf61b47c82c
dvberkel <daan.v.berkel.1980@gmail.com>
2014-12-18 16:05:35 +0100

    create a gulpFile to compress sources

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function(){
 	gulp.src('fps.js')
-
+		.pipe(uglify())
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('./'));
 });
